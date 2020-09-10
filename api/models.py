cfea9d87955b56ea470fb90c339838815413bf05
nyraonline <amitengineer.patel@gmail.com>
2020-09-10 22:30:12 +0530

    something

--- api/models.py
+++ api/models.py
@@ -5,7 +5,7 @@ import django
 
 # Create your models here.
 class Emp(models.Model):
-    id=models.IntegerField(auto_created=True,primary_key=True)
+    id = models.IntegerField(auto_created=True, primary_key=True)
     name = models.CharField(max_length=100, default='none')
     email = models.EmailField(default='none')
     username = models.CharField(max_length=100, default='undefined')
